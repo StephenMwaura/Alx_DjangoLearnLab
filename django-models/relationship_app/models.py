0898227d79e52c1782c592eabedcda5d012ff109
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-15 12:36:25 +0300

    corrections

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -6,11 +6,10 @@ class Author (models.Model):
     
     def __str__(self):
         return self.name
-class Book (models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    def __str__(self):
-        return {self.title} ,{self.author} 
+    
     
 class Library(models.Model):
     name = models.CharField(max_length=100)
