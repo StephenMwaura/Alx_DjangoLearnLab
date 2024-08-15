59b9c1a46dc15428bc314bd0e84c92663645649e
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-15 13:07:45 +0300

    corrections

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from .models import Author, Book, Library , Librarian
 def Query_all_books_by_a_specific_author(author_name):
  try:
   
-   author = Author.objects.get(name = author_name)
+   author = Author.objects.get(name=author_name)
    books = Book.objects.filter(author=author)
    return books
  except :
@@ -14,7 +14,8 @@ def List_all_books_in_a_library(library_name):
   books = library.books.all()
   return books
     
-def librarian_for_library(library_name):
-   lib = Library.objects.get(name= library_name)
-   libra = Librarian.objects.get(library = lib)
-   return libra
+def Retrieve_the_libranian_for_a_library(library_name):
+   library = Library.objects.get(name=library_name)
+
+   libranian = Librarian.objects.get(library=library)
+   return Librarian
