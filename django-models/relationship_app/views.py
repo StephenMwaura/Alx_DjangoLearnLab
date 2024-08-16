2d2c68b7375144e0245f2251539fa94bfc205868
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-16 18:02:00 +0300

    corrections

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -8,8 +8,7 @@ def lists_all_books(request):
     return render(request ,'relationship_app/list_books.html', Book.objects.all() )
 class display(ListView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = 'library_detail.html', 'from .models import Library'
 
 def get_context_data(self, **kwargs):
     context = super().get_context_data(**kwargs) 
