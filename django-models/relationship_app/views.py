12ae03f5b177430f8b034c5dbf0ef5a1b8389829
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-16 17:48:56 +0300

    added corrections

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,27 @@
+from typing import Any
 from django.shortcuts import render
+from .models import Book , Library
+from django.views.generic import ListView
 
 # Create your views here.
+def lists_all_books(request):
+    return render(request ,'list_books.html', Book.objects.all() )
+class display(ListView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs) 
+    context['books'] = self.objects.books.all()
+    return context
+
+
+
+
+        
+    
+ 
+
+
+
