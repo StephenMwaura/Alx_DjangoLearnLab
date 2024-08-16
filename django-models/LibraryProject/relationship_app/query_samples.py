e64fc4c3d21adbe7d25a9fd76c71abfc1ae0ac33
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-16 17:43:00 +0300

    new files

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,18 +1,17 @@
-from relationship_app.models import Author, Book, Library , Librarian
+from .models import Author, Book, Library , Librarian
 
-def query_by_author(author_name):
- try:
-  
-   author = Author.objects.get(name = author_name)
-   books = Book.objects.filter(author = author)
-   return books
- except :
-   return f"Author does not exist"
+def Query_all_books_by_a_specific_author(author_name):
+  author= Author.objects.get(name=author_name)
+  books = Book.objects.filter(author=author)
+  return books
 
-def list_books():
-    library_books = Book.objects.all()
-    return library_books
-def librarian_for_library(library_name):
-   lib = Library.objects.get(name= library_name)
-   libra = Librarian.objects.get(library = lib)
-   return libra
+def List_all_books_in_a_library(library_name):
+  library = Library.objects.get(name=library_name)
+  books = library.books.all()
+  return books
+    
+def Retrieve_the_libranian_for_a_library(library_name):
+   library = Library.objects.get(name=library_name)
+
+   libranian = Librarian.objects.get(library=library)
+   return Librarian
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,29 @@
+from typing import Any
 from django.shortcuts import render
+from .models import Book , Library
+from django.views.generic import ListView
 
 # Create your views here.
+def lists_all_books(request):
+    books = Book.objects.all()
+    context = {'lists_all_books':books}
+    return render(request ,'list_books.html', context )
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs) 
+    context['books'] = self.objects.books.all()
+    return context
+
+
+
+
+        
+    
+ 
+
+
+
