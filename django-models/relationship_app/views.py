b17c0e9687fd9a696bac9bed1a4ab0f26f6e8351
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 10:21:00 +0300

    corrections

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,33 +1,44 @@
+from django.contrib.auth import login
 from typing import Any
 from django.shortcuts import render
 from .models import Book , Library
-from django.views.generic.detail import DetailView , CreateView
+from django.views.generic import DetailView , CreateView 
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.contrib.auth.views import LoginView , LogoutView
+from django.contrib.auth.views import LoginView , LogoutView 
+
+
 from django.urls import path
-
-
 # Create your views here.
 def lists_books(request):
-    return render(request ,'relationship_app/list_books.html', Book.objects.all() )
+    books = Book.objects.all()
+    context = {'lists_all_books':books}
+    return render(request ,'list_books.html', context )
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html', 'from .models import Library'
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+     context = super().get_context_data(**kwargs) 
+     context['books'] = self.objects.books.all()
+     return context
 
-def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs) 
-    context['books'] = self.objects.books.all()
-    return context
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    sucess_url = reverse_lazy('login')
+    success_url = reverse_lazy('login')
     template_name = 'register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('logout/',LogoutView.as_view(), name = 'logout.html')
+    path('logout/',LogoutView.as_view(), name = 'logout'),
+    path('register/', SignUpView.as_view(), name='register'),
 ]
 
 
