1ab20d32ff2a503c51886604612841f5342d127a
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 10:16:43 +0300

    corrections

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,21 +17,27 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
-
-def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs) 
-    context['books'] = self.objects.books.all()
-    return context
+    
+    def get_context_data(self, **kwargs):
+     context = super().get_context_data(**kwargs) 
+     context['books'] = self.objects.books.all()
+     return context
 
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    sucess_url = reverse_lazy('login')
+    success_url = reverse_lazy('login')
     template_name = 'register.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
-    path('logout/',LogoutView.as_view(), name = 'logout.html')
+    path('logout/',LogoutView.as_view(), name = 'logout'),
+    path('register/', SignUpView.as_view(), name='register'),
 ]
 
 
