f4a151af10073c24d2ef4ffc75bbfc1a9297e9a8
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 11:00:02 +0300

    corrections

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -26,9 +26,9 @@ class LibraryDetailView(DetailView):
 
 
 class  register(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationForm()
     success_url = reverse_lazy('login')
-    template_name = 'register.html'
+    template_name = 'relationship_app/register.html'
 
     def form_valid(self, form):
         user = form.save()
@@ -39,6 +39,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('logout/',LogoutView.as_view(), name = 'logout'),
     path('register/',register.as_view(), name='register'),
+    
 ]
 
 
