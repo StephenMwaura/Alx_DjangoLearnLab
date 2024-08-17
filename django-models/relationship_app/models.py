cce0c7a8b92547bac578102e29578122e853bb14
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 20:07:50 +0300

    corrections

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -44,12 +44,12 @@ def user_profile(sender, instance, created, **kwargs):
     else:
         instance.userprofile.save()
 
-from  django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponse
+# from  django.contrib.auth.decorators import user_passes_test
+# from django.http import HttpResponse
 
-def is_Admin(user):
-    return user.is_authenticated and user.groups.filter(name='Admin').exists()
-@user_passes_test(is_Admin)
-def Admin_view(request):
-    return HttpResponse("Welcome, Admin")
+# def is_Admin(user):
+#     return user.is_authenticated and user.groups.filter(name='Admin').exists()
+# @user_passes_test(is_Admin)
+# def Admin_view(request):
+#     return HttpResponse("Welcome, Admin")
 
