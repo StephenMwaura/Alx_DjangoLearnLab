352ec63abe899af70ba6ad5ca260986b9bf8232e
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 10:27:22 +0300

    corrections

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("books/", lists_books,name='list_books'),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name='library_list'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+
     path('register/', SignUpView.as_view(), name='register'),
 ]
