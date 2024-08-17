5d33d1fc1a76ca197d7adb77712605e0eec61daa
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 19:59:52 +0300

    task 3 files

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -24,3 +27,19 @@ class Librarian(models.Model):
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
     def __str__(self):
         return {self.name} , {self.library}
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.Cascade)
+    role_choices = [
+         ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+
+    ]
+role = models.CharField(max_length=20, choices='role_choices')
+
+def user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    else:
+        instance.userprofile.save()
--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,6 +3,9 @@ from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books 
 from .views import LibraryDetailView
 from .import views
+from .admin_view import admin_view
+from .librarian_view import librarian_view
+from .member_view import member_view
 urlpatterns = [
     path("books/", list_books,name='list_books'),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name='library_list'),
@@ -10,4 +13,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 
     path('register/', views.register.as_view(), name='register'),
+    path('admin/', admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
