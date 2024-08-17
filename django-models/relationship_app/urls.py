7ff695ca6a97d409b4a5f1dfdc46fecd0262ffa8
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 10:34:30 +0300

    corrections

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView 
-from .views import lists_books , LibraryDetailView, RegisterView
+from .views import lists_books , LibraryDetailView, register
 
 urlpatterns = [
     path("books/", lists_books,name='list_books'),
@@ -8,5 +8,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/', register.as_view(), name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -25,7 +25,7 @@ class LibraryDetailView(DetailView):
      return context
 
 
-class  RegisterView(CreateView):
+class  register(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'register.html'
@@ -38,7 +38,7 @@ class  RegisterView(CreateView):
 urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('logout/',LogoutView.as_view(), name = 'logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+    path('register/',register.as_view(), name='register'),
 ]
 
 
