4f729f4c50f8d188b4ced430f35fde30a9e1bf7a
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 10:51:36 +0300

    corrections

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView 
-from .views import lists_books , LibraryDetailView, register
-
+from .views import lists_books , LibraryDetailView
+from .import views
 urlpatterns = [
     path("books/", lists_books,name='list_books'),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name='library_list'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 
-    path('register/', register.as_view(), name='register'),
+    path('register/', views.register.as_view(), name='register'),
 ]
