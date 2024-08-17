c6da52d27b53c758f5fc2eb05c8015f1adf7f536
Stephen Mwaura <stevecartel44@gmail.com>
2024-08-17 20:29:13 +0300

    corrections

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,9 @@ from django.views.generic.detail import DetailView , CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView , LogoutView 
+from .templates.relationship_app import Admin_view
+from .templates.relationship_app import librarian_view
+from .templates.relationship_app import member_view
 
 
 from django.urls import path
@@ -40,7 +43,9 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
     path('logout/',LogoutView.as_view(), name = 'logout'),
     path('register/',register.as_view(), name='register'),
-    
+    path('admin/', Admin_view, name='admin_view'),
+    path('librarian/', librarian_view, name='librarian_view'),
+    path('member/', member_view, name='member_view'),
 ]
 
 
