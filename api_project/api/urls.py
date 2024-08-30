fc8f528b598441ca2a905f4d4607588268d6affa
Derrick Muturi <wawerumuturi57@gmail.com>
2024-08-30 10:25:51 +0300

    corrections on task 1

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from views import BookList
 
 urlpatterns = [
-    path("", views.BookList.as_view(), name="book_list_view"),
+    path("", BookList.as_view(), name="book_list_view"),
 
 ]
