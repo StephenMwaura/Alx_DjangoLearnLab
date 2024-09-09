35aa47d1bfd1a91bc5ea7e4e3424709abbd20113
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-09 23:11:08 +0300

    task 0

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'mwauramungai2006$',
+        'HOST': 'localhost',
+        'PORT': '3306'
+       
     }
 }
 
@@ -117,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
