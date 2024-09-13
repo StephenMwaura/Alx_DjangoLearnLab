733ced351267e97ae9b80777660133df818edc01
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-13 23:20:35 +0300

    stage changes for task 2 files

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.decorators import  login_required
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.forms import UserCreationForm
