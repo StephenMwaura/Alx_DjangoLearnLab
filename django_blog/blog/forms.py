c12633a72cb59e6561fa08b1db0d89d3d97e2614
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-14 22:16:27 +0300

    task 4 files

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -41,3 +41,6 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tags']
+        widgets = {
+            'title':forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Enter post title'})
+        }
