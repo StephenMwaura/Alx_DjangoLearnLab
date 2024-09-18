21b3ffacca1ec4509a4faa2baed60259702ad169
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-18 22:16:18 +0300

    task 1 corrections

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
-    title = models.TextField(max_length=50)
+    title = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
 
