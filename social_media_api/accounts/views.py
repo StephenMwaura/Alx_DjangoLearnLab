d7e8bbb4b7223d8a3ce83ea76e99f8a9dbba13e7
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-18 15:50:16 +0300

    task 0 files

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -8,16 +8,19 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework import status
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = RegisterSerializer
-
-    def create(self, request, *args, **kwargs):
-        response = super().create(request , *args , **kwargs)
-        user = CustomUser.objects.get(username=response.data['username'])
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key})
+        def post(self , request):
+             serializer = RegisterSerializer(data=request.data)
+             if serializer.is_valid():
+                  user = serializer.save()
+                  token , created = Token.objects.get_or_create(user=user)
+                  return Response({
+                       "user": serializer.data,
+                       "token":token.key
+                  },status=status.HTTP_201_CREATED)
+             return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
 
 # allows users to retrieve and update their profile
 class ProfileView(generics.RetrieveUpdateAPIView):
@@ -32,14 +35,14 @@ class ProfileView(generics.RetrieveUpdateAPIView):
         return f"Welcome you are an authenticated user."
 
 class LoginView(APIView):
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(username=username , password=password)
         if user:
             token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key})
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
         else:
-            return Response({'Invalid credentilas'}, status=400)
+            return Response({'Invalid credentilas'}, status=status.HTTP_400_BAD_REQUEST)
 
         
