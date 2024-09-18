385991af1f531b6da7ac77f9999c5af729078c60
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-18 16:55:49 +0300

    changes to task 0 files

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated , AllowAny
 from rest_framework.views import APIView
 from django.views.generic import CreateView
 from .models import CustomUser
@@ -8,19 +8,23 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import status
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
-        def post(self , request):
-             serializer = RegisterSerializer(data=request.data)
-             if serializer.is_valid():
-                  user = serializer.save()
-                  token , created = Token.objects.get_or_create(user=user)
-                  return Response({
-                       "user": serializer.data,
-                       "token":token.key
-                  },status=status.HTTP_201_CREATED)
-             return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+    
+    def post(self , request):
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+                token , created = Token.objects.get_or_create(user=user)
+                return Response({
+                    "user": serializer.data,
+                    "token":token.key
+                },status=status.HTTP_201_CREATED)
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
 
 # allows users to retrieve and update their profile
 class ProfileView(generics.RetrieveUpdateAPIView):
@@ -34,15 +38,8 @@ class ProfileView(generics.RetrieveUpdateAPIView):
     def get(self ,request):
         return f"Welcome you are an authenticated user."
 
-class LoginView(APIView):
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(username=username , password=password)
-        if user:
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
-        else:
-            return Response({'Invalid credentilas'}, status=status.HTTP_400_BAD_REQUEST)
-
-        
+class LoginView(ObtainAuthToken):
+     def post(self,request, *args, **kwargs):
+          response = super(LoginView, self).post(request, *args, **kwargs)
+          token = Token.objects.get(key=response.data['token'])
+          return Response({'token': token.key}, status=status.HTTP_200_OK)
