a3a5650295125b53e7bcff9554f5034c6837c554
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-18 15:52:44 +0300

    task 0 files

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,22 +3,22 @@ from .models import CustomUser
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User # customUser
+        model = get_user_model # customUser
         fields = ('username','bio', 'profile_picture','followers')
 
 # This serializer handles user registration ensures password is write only wont be returned in the response
 class RegisterSerializer(serializers.ModelSerializer):
     password  = serializers.CharField(required=True, write_only=True)
     class Meta:
-        model = User
+        model = get_user_model
         fields = ('username','email','password')
 
         def create(self,validated_data):
-            user = User.objects.create_user( # the create_user handles the password hashing
+            user = get_user_model.objects.create_user( # the create_user handles the password hashing
                 username=validated_data['username'],
                 email=validated_data['email'],
                 password=validated_data['password'],
