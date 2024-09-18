daf66a723657df42f3e41ac50634f48dc53c1945
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-18 22:15:21 +0300

    task 1 corrections

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -3,15 +3,15 @@ from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    created_at = models.TimeField(auto_now_add=True)
-    updated_at = models.TimeField(auto_now_add=True)
+    title = models.TextField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post , on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL , on_delete=models.CASCADE)
     content = models.CharField(max_length=100)
-    created_at = models.TimeField(auto_now_add=True)
-    updated_at = models.TimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     
