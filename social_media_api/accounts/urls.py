bf51d3954b4d714f907b1840bb1072970e9e1506
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-20 21:22:10 +0300

    corrections on task 2

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<str:username>/', FollowView.as_view(), name='follow_user'),
-    path('unfollow/<str:username>/', UnfollowView.as_view(), name='unfollow_user'),
+    path('follow/<int:user_id>/', FollowView.as_view(), name='follow_user'),
+    path('unfollow/<int:user_id>/', UnfollowView.as_view(), name='unfollow_user'),
 ]
