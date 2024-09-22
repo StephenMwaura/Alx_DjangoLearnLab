deab7ee8304af99eca72171855d406bedb10c0c2
Stephen Mwaura <stevecartel44@gmail.com>
2024-09-22 21:02:10 +0300

    task 4 files

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'deploy',
+        'USER': 'root',
+        'PASSWORD': 'mwauramungai2006$',
+        'HOST': 'localhost',
+        'PORT': '3306'
+        
+
     }
 }
 
